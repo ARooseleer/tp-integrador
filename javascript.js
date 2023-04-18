70ce5383a6ceca1e6558dc9675ab189331b04f81
Juanmd14 <111128350+Juanmd14@users.noreply.github.com>
2023-04-18 12:51:31 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function convertirPesosABitcoin() {
     var pesos = document.getElementById("pesos").value;
-    var bitcoin = pesos / 4640000; // tipo de cambio aproximado
+    var bitcoin = pesos / 6500000; // tipo de cambio aproximado
     document.getElementById("bitcoin").value = bitcoin.toFixed(8);
     return false;
   }
